6b52195be65c4989725319a1858132e390e8d821
Mauricio Cesar Rojas <mauriciocc.rojas@gmail.com>
2021-02-02 12:39:02 -0300

    prueba 2-1

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -4,7 +4,7 @@ y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y 
 se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar 
-cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+cuantas bolsas se necesitan de cada uno para las medidas que nos ingreseen.
 */
 
 function Rectangulo() {
