8b351f91d7cc8138ff3a4895970f1351975547c0
Mauricio Cesar Rojas <mauriciocc.rojas@gmail.com>
2021-02-18 23:14:04 -0300

    cambios 18febrero2020

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,49 +1,36 @@
 function mostrar() {
-	let estacion;
-	estacion = document.getElementById("txtIdEstacion").value;
-	destino = document.getElementById("txtIdDestino").value;
+  let estacion;
+  estacion = document.getElementById("txtIdEstacion").value;
+  destino = document.getElementById("txtIdDestino").value;
+ mensaje = "No se viaja";
+  switch (estacion) {
+    case "Invierno":
+      if (destino == "Bariloche") {
+        alert("Se viaja");
+      } else {
+        alert("No se viaja");
+      }
+      break;
 
+    case "Otoño":
+      alert("Se viaja");
 
+      break;
 
-	switch (estacion) {
-		case "Invierno":
-			if (destino == "Bariloche") {
-				alert("Se viaja");
-			}
-			else {
-				alert("No se viaja");
-			}
-			break;
+    case "Verano":
+      if (destino == "Cataratas" || destino == "Mar del plata") {
+        alert("Se viaja");
+      } else {
+        alert("No se viaja");
+      }
+      break;
 
-		case "Otoño":
-		//	if (destino == "Cataratas" || destino == "Mar del plata" || destino == "Bariloche" || destino == "Cordoba") { // muy largo
-			if (destino != "doba") { // asi más facil, porque digo que si no es por ej "doba", todo sí
-				alert("Se viaja");
-			}
-			break;
-
-		case "Verano":
-			if (destino == "Cataratas" || destino == "Mar del plata") {
-				alert("Se viaja");
-			}
-			else {
-				alert("No se viaja");
-			}
-			break;
-
-		case "Primavera":
-			if (destino != "Bariloche") {
-				alert("Se viaja");
-		
-			}
-			else {
-				alert("No se viaja");
-			}
-			break;
-
-
-
-
-
-	}
-}//FIN DE LA FUNCIÓN
+    case "Primavera":
+      if (destino != "Bariloche") {
+        alert("Se viaja");
+      } else {
+        alert("No se viaja");
+      }
+      break;
+  }
+} //FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,43 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+function mostrar() {
+  let numero;
+  let maximo;
+  let minimo;
+  let respuesta;
+  let flag = 0;
+
+  do {
+    numero = parseInt(prompt("Ingrese un numero"));
+    if (flag == 0) {
+      maximo = numero;
+      minimo = numero;
+      flag = 1;
+    } else if (numero < minimo) {
+      minimo = numero;
+    } else if (numero > maximo) {
+      maximo = numero;
+    }
+    respuesta = prompt("¿Quiere ingresar otro numero?");
+  } while (respuesta == `s`);
+
+  document.getElementById("txtIdMaximo").value = maximo;
+  document.getElementById("txtIdMinimo").value = minimo;
+}
+
+
+/*
+if (flag == 0 || numero < minimo) {
+	maximo = numero;
+}
+if (flag == 0 || numero > minimo) {
+	minimo = numero;
+	flag = 1;
+}
+*/ //usar este
+
+
+// while cuando no sé la cantidad de repeticiones
+// for cuando la conozco
+// do wile cuando el codigo se tiene que ejecutar al menos una vez
